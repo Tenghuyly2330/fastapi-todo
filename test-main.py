d1b8e2e8aa50685e4b3076afd5ada081272429b4
Tenghuy <lytenghuy8@gmail.com>
2026-05-29 13:49:21 +0700

    add tests

---
+++ test-main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient()
+
+def test_home():
+    response = client("/")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "FastApi CI/CD App"
+    }
